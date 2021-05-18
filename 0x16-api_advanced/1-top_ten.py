20401f30c325d97b06f60780919bc0e72164638c
yagomfh <yagomfh@gmail.com>
2021-05-18 10:03:38 +0000

    fix: typo

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,10 +8,10 @@ def top_ten(subreddit):
     headers = {'User-agent': 'Unix:0-subs:v1'}
     url = ("https://www.reddit.com/r/" +
            subreddit +
-           "/hot/.json?count=10&limit=10")
+           "/hot/.json?limit=10")
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code != 200 or not data:
         print("None")
-        return
-    for elem in response.json()['data']['children']:
-        print(elem['data']['title'])
+    else:
+        for elem in response.json()['data']['children']:
+            print(elem['data']['title'])
